f6118ddc494ab48b40f8309dc93dd66d0e43ae37
Akn <sibata486@gmail.com>
2023-11-02 22:24:40 +0900

    add / FeedReader.GetRss() で値を返すため、Feed.cs を追加

---
+++ RssReader/Models/Feed.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace RssReader.Models
+{
+    public class Feed
+    {
+        public int Id { get; set; }
+
+        public int ParentId { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Description { get; set; }
+
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public bool IsRead { get; set; }
+    }
+}
