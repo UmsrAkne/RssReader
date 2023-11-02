c7743c43340c08496feacd365ecb0e8fe04d1004
Akn <sibata486@gmail.com>
2023-11-02 21:39:53 +0900

    init

---
+++ RssReader/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Prism.Mvvm;
+
+namespace RssReader.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class MainWindowViewModel : BindableBase
+    {
+        public string Title => "Prism Application";
+    }
+}
