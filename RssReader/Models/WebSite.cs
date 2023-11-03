feb06efdc7b7bbee0aebf59b0daa4cd3443a251f
Akn <sibata486@gmail.com>
2023-11-03 15:57:26 +0900

    add / Database の Entity クラスの文字列プロパティにデフォルト値を追加

--- RssReader/Models/WebSite.cs
+++ RssReader/Models/WebSite.cs
@@ -12,12 +12,12 @@ namespace RssReader.Models
         ///     サイトの表示名
         /// </summary>
         [Required]
-        public string Title { get; set; }
+        public string Title { get; set; } = string.Empty;
 
         /// <summary>
         ///     ウェブサイトの RSS 配信の URL
         /// </summary>
         [Required]
-        public string Url { get; set; }
+        public string Url { get; set; } = string.Empty;
     }
 }
