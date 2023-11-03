b616166a37ca46d46e8f4a11b5f1ed9cba113224
Akn <sibata486@gmail.com>
2023-11-03 23:22:03 +0900

    add feat / データベースから WebSiteWrapper のリストを習得する処理を実装

--- RssReader/Models/Databases/DatabaseManager.cs
+++ RssReader/Models/Databases/DatabaseManager.cs
@@ -26,5 +26,19 @@ namespace RssReader.Models.Databases
         {
             return DataSource.GetFeeds().Where(f => f.ParentSiteId == webSiteId);
         }
+
+        public IEnumerable<WebSiteWrapper> GetWebSiteWrappers()
+        {
+            return DataSource.GetWebSites()
+                .GroupBy(g => g.GroupId)
+                .Select(group =>
+                {
+                    var wg = DataSource.GetWebSiteGroups().FirstOrDefault(w => w.Id == group.Key);
+                    return new WebSiteWrapper(wg)
+                    {
+                        Children = group.Select(w => new WebSiteWrapper(w)).ToList(),
+                    };
+                });
+        }
     }
 }
