3cbaa5d83139fd5485e06c9a387ccf626ae644d5
Akn <sibata486@gmail.com>
2024-02-04 22:49:34 +0900

    add ui / WebSiteGroupEditPage に最低限のコントロールを設置

--- RssReader/ViewModels/WebSiteGroupEditPageViewModel.cs
+++ RssReader/ViewModels/WebSiteGroupEditPageViewModel.cs
@@ -2,20 +2,27 @@ using System;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Services.Dialogs;
+using RssReader.Models.Databases;
 
 namespace RssReader.ViewModels
 {
     // ReSharper disable once ClassNeverInstantiated.Global
     public class WebSiteGroupEditPageViewModel : BindableBase, IDialogAware
     {
+        private string groupName;
+
         public event Action<IDialogResult> RequestClose;
 
         public string Title => string.Empty;
+
+        public string GroupName { get => groupName; set => SetProperty(ref groupName, value); }
 
         public DelegateCommand CloseCommand => new (() =>
         {
             RequestClose?.Invoke(new DialogResult());
         });
+
+        private DatabaseManager DatabaseManager { get; set; }
 
         public bool CanCloseDialog()
         {
@@ -28,6 +35,7 @@ namespace RssReader.ViewModels
 
         public void OnDialogOpened(IDialogParameters parameters)
         {
+            DatabaseManager = parameters.GetValue<DatabaseManager>(nameof(DatabaseManager));
         }
     }
 }
